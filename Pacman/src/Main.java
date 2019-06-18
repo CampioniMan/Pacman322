4cd41fcdaba3f551ef52bedc30adea742df2e55f
Daniel Samogin Campioni <ra214777@correa.lab.ic.unicamp.br>
2019-06-18 20:09:27 -0300

    Adicionado colisores para facilitar a nossa vida

--- Pacman/src/Main.java
+++ Pacman/src/Main.java
@@ -1,12 +1,8 @@
-import java.io.File;
-
-import com.unicamp.mc322.mapa.Mapa;
 import com.unicamp.mc322.pacman.Game;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
 		Game game = new Game();
 		game.start();
 	}
