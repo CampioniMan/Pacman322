49f50fbd12a93045e902eee55605b6291c24ba7e
Dorival Alves da Silva Junior <ra215076@beatles.lab.ic.unicamp.br>
2019-06-19 21:54:45 -0300

    AGORA SIM

---
+++ Pacman/src/com/unicamp/mc322/pacman/personagem/fantasma/FantasmaPrestigiador.java
@@ -0,0 +1,44 @@
+package com.unicamp.mc322.pacman.personagem.fantasma;
+
+import java.util.Random;
+
+import com.unicamp.mc322.pacman.colisao.ColisorQuadrado;
+import com.unicamp.mc322.pacman.posicionamento.Direcao;
+import com.unicamp.mc322.pacman.posicionamento.Quadrado;
+import com.unicamp.mc322.parede.ParedeController;
+
+public class FantasmaPrestigiador extends Fantasma{
+	private Integer countPos = 0;
+	public FantasmaPrestigiador(Quadrado colider) {
+		super(colider, "src/sprites/ghosts/fantasmaamarelo1.png");
+	}
+
+	private ColisorQuadrado calcularNovoColider() {
+		Integer X = ((new Random()).nextInt(30)+1)*16;
+		Integer Y = ((new Random()).nextInt(30)+1)*16;
+		return new ColisorQuadrado(X,Y ,X + 16,Y +16);
+	}
+	@Override
+	public void calculaPosicaoNova(ParedeController paredeController) {
+		
+		if (countPos++ > 10 ) {
+		
+			ColisorQuadrado colisorQuadrado = calcularNovoColider();
+			if (paredeController.colidiuComQuadrado(colisorQuadrado))
+			{
+				calculaPosicaoNova(paredeController);
+			}
+			else
+			{
+
+				this.colider.getAreaTotal().getTopoEsquerdo().setX(colisorQuadrado.getAreaTotal().getTopoEsquerdo().getX());
+				this.colider.getAreaTotal().getTopoEsquerdo().setY(colisorQuadrado.getAreaTotal().getTopoEsquerdo().getY());
+				this.colider.getAreaTotal().getValeDireito().setX(colisorQuadrado.getAreaTotal().getValeDireito().getX());
+				this.colider.getAreaTotal().getValeDireito().setY(colisorQuadrado.getAreaTotal().getValeDireito().getY());
+			}
+			countPos = 0;
+		}
+	}
+	
+	
+}
